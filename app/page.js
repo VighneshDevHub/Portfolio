3fdf647c5c459c5f6c736191857f53a77abce80c
Vighnesh Salunkhe <vighneshsalunkhe949@gmail.com>
2025-07-22 19:49:12 +0530

    feat: Add all sections in homepage

--- app/page.js
+++ app/page.js
@@ -1,21 +1,22 @@
 import Hero from "../components/Hero"
 import AboutSection from "../components/AboutSection"
-import ExperienceSection from "../components/ExperienceSection"
 import ProjectsSection from "../components/ProjectsSection"
 import BlogSection from "../components/BlogSection"
 import ContactSection from "../components/ContactSection"
 import Testimonials from "../components/Testimonials"
+import SkillSection from "@/components/SkillSection"
+import GetInTouch from "@/components/GetInTouch"
 
 export default function Home() {
   return (
   <>
     <Hero />
     <AboutSection />
-    <ExperienceSection />
     <ProjectsSection />
+    <SkillSection/>
     <BlogSection />
-    <ContactSection />
     <Testimonials />
+    <GetInTouch />
 
   </>
     
