317ecedffb73c452b826330a82ed7df9b06f3906
Vighnesh Salunkhe <vighneshsalunkhe949@gmail.com>
2025-07-24 09:46:16 +0530

    Add: All routes inside page.js

--- app/page.js
+++ app/page.js
@@ -6,6 +6,7 @@ import ContactSection from "../components/ContactSection"
 import Testimonials from "../components/Testimonials"
 import SkillSection from "@/components/SkillSection"
 import GetInTouch from "@/components/GetInTouch"
+import CTA from '@/components/CTA'
 
 export default function Home() {
   return (
@@ -16,7 +17,7 @@ export default function Home() {
     <SkillSection/>
     <BlogSection />
     <Testimonials />
-    <GetInTouch />
+    <CTA />
 
   </>
     
