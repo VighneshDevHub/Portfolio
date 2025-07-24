bd21aa8eaa9aba6611c2de6d49738999c74f1762
Vighnesh Salunkhe <vighneshsalunkhe949@gmail.com>
2025-07-24 09:40:52 +0530

    Add: /blogs route

--- app/blogs/page.js
+++ app/blogs/page.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import Hero from './Hero'
+import BlogSection from './BlogSection'
+import CTA from '@/components/CTA'
 
 const page = () => {
   return (
-    <div>
-      Blogs
-    </div>
+    <>
+    <Hero/>
+    <BlogSection/>
+    <CTA/>
+    </>
   )
 }
 
